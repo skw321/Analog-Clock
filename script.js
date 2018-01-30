64eb870108125a73b9b19c51d1e9e52f55e5155d
skw321 <skwuslife@gmail.com>
2018-01-30 03:44:58 -0600

    Commit For Clock working with Javascript

--- script.js
+++ script.js
@@ -1,3 +1,22 @@
 const HOURHAND = document.querySelector("#hour");
 const MINUTEHAND = document.querySelector("#minute");
 const SECONDHAND = document.querySelector("#second");
+
+
+var date = new Date();
+
+let hr = date.getHours();
+let min = date.getMinutes();
+let sec = date.getSeconds();
+
+console.log(hr+min+sec);
+
+let hrPosition = 30*hr+(0.5*min)+(0.1/12*sec);
+let minPosition = (6*min)+(0.1*sec);
+let secPosition = 6*sec;
+
+HOURHAND.style.transform = "rotate(" + hrPosition + "deg)";
+MINUTEHAND.style.transform = "rotate(" + minPosition + "deg)";
+SECONDHAND.style.transform = "rotate(" + secPosition + "deg)";
+
+
