144a53d9ca19c862d9fad25d8b55e8f10add5d4e
skw321 <skwuslife@gmail.com>
2018-01-30 03:57:05 -0600

    Final Commit For Working Clock

--- script.js
+++ script.js
@@ -2,7 +2,6 @@ const HOURHAND = document.querySelector("#hour");
 const MINUTEHAND = document.querySelector("#minute");
 const SECONDHAND = document.querySelector("#second");
 
-
 var date = new Date();
 
 let hr = date.getHours();
@@ -15,8 +14,19 @@ let hrPosition = 30*hr+(0.5*min)+(0.1/12*sec);
 let minPosition = (6*min)+(0.1*sec);
 let secPosition = 6*sec;
 
-HOURHAND.style.transform = "rotate(" + hrPosition + "deg)";
-MINUTEHAND.style.transform = "rotate(" + minPosition + "deg)";
-SECONDHAND.style.transform = "rotate(" + secPosition + "deg)";
+
+function runTheClock(){
+
+    hrPosition+=1/120;
+    minPosition+=0.1;
+    secPosition+=6;
+    
+    HOURHAND.style.transform = "rotate(" + hrPosition + "deg)";
+    MINUTEHAND.style.transform = "rotate(" + minPosition + "deg)";
+    SECONDHAND.style.transform = "rotate(" + secPosition + "deg)";
+}
+
+var interval = setInterval(runTheClock,1000);
+
 
 
